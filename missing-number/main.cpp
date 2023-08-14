7edf23b5542179dd7b7d85eb984e649fd54ea86a
Hudson Luiz Sales Schumaker <hudson.schumaker@me.com>
2023-08-14 21:02:41 +0200

    Update main.cpp

--- missing-number/main.cpp
+++ missing-number/main.cpp
@@ -1,1 +1,12 @@
-
+#include <bits/stdc++.h>
+ 
+int main() {
+    long long n, s = 0;
+    std::cin >> n;
+    for(long long i = 1; i < n; ++i) {
+        long long a;
+        std::cin >> a;
+        s += a; 
+    }
+     std::cout << n*(n + 1)/2 - s;
+}
